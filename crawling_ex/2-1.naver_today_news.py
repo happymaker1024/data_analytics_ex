2d189aa9f203ddfdaa83761aea225e587ecbc139
happymaker <happymaker1024@gmail.com>
2025-11-17 14:38:34 +0900

    네이버 뉴스 수집

--- crawling_ex/2-1.naver_today_news.py
+++ crawling_ex/2-1.naver_today_news.py
@@ -15,24 +15,21 @@ elem_list = soup.select('ul.sa_list li.sa_item')
 # news headline 만 추출
 searchword_list = []
 for elem in elem_list:
-    one_news = []
     # print(elem)
     head_line = elem.select_one('.sa_text_strong').get_text().strip()
-    one_news.append(head_line)
     newspaper = elem.select_one('.sa_text_press').get_text().strip()
-    one_news.append(newspaper)
     # 이미지 src 추출 (.sa_thumb_inner 아래의 img 태그)
     img_url = elem.select_one('.sa_thumb_inner img')
     # lazy-loading일 경우 data-src 등에 있을 수 있으므로 우선순위로 가져옴
     if img_url:
         img_url = img_url.get('src') or img_url.get('data-src') or ''
         img_url = img_url.split('?')[0]  # URL 파라미터 제거
-        one_news.append(img_url)
     else:
-        one_news.append('img url 없음')
+        img_url = 'img url 없음'
+    # 터미널 출력
     print(f"{head_line} - {newspaper}\nImage URL: {img_url}")
-
-    searchword_list.append(head_line)
+    # list에 추가 : 뉴스 한 개에 대한 수집 정보
+    searchword_list.append([head_line, newspaper, img_url])
 
 # 저장할 파일 이름 구하기
 base_path = 'naver_news/'
